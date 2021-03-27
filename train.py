00dd8b04a2a395fdddb350e7ecede2540ae350d5
Fumi <fnakashima@users.noreply.github.com>
2021-03-27 19:56:38 +0000

    finished hyperdrive run

--- train.py
+++ train.py
@@ -20,15 +20,6 @@ ds = TabularDatasetFactory.from_delimited_files(path=web_path)
 
 #ds.to_pandas_dataframe()
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-# https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
-# Default test_size: 0.25
-x_train, x_test, y_train, y_test = train_test_split(x, y)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -58,6 +49,15 @@ def clean_data(data):
     return x_df, y_df
     
 
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+# https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+# Default test_size: 0.25
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+run = Run.get_context()
+
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
@@ -67,8 +67,8 @@ def main():
 
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
+    run.log("Regularization Strength(C)", np.float(args.C))
+    run.log("Max iterations", np.int(args.max_iter))
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
